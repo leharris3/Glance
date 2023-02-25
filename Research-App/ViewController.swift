044b46cab02b22e6c025c08201b99e997abeca67
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-02-25 12:36:47 -0500

    Initial Commit

---
+++ Research-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Research-App
+//
+//  Created by Levi Harris on 2/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
