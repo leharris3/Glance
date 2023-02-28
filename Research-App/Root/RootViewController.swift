385b6f12f0c61918365d39dc4fc684ebf097fbf7
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-02-28 13:58:32 -0500

    Front page

--- Research-App/Root/RootViewController.swift
+++ Research-App/Root/RootViewController.swift
@@ -14,19 +14,8 @@ class RootViewController: UIViewController, FUIAuthDelegate {
 //
 //    }
     
-    @IBOutlet weak var signupButton: UIButton!
-    
-    @IBOutlet weak var signinButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        signupButton.layer.borderWidth = 1.5
-        signupButton.layer.borderColor = UIColor.white.cgColor
-        signupButton.layer.cornerRadius = 24
-        
-        signinButton.layer.borderWidth = 1.5
-        signinButton.layer.borderColor = UIColor.white.cgColor
-        signinButton.layer.cornerRadius = 24
         
         if Auth.auth().currentUser != nil {
 
