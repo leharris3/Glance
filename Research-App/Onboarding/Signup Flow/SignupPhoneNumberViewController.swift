edbaeba411bdd4ee644b24eb96bdaa15c8a3e5f6
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-03-04 14:33:38 -0500

    Adjusts views on keyboard popup functionality

--- Research-App/Onboarding/Signup Flow/SignupPhoneNumberViewController.swift
+++ Research-App/Onboarding/Signup Flow/SignupPhoneNumberViewController.swift
@@ -8,44 +8,45 @@
 import UIKit
 
 class SignupPhoneNumberViewController: UIViewController {
+
+    @IBOutlet weak var fieldsViewBottomConstraint: NSLayoutConstraint!
     
-    @IBOutlet var keyboardHeightLayoutConstraint: NSLayoutConstraint?
-    //
+    @IBOutlet weak var fieldsView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //
-        //        NotificationCenter.default.addObserver(self,
-        //                                               selector: #selector(self.keyboardNotification(notification:)),
-        //                                               name: UIResponder.keyboardWillChangeFrameNotification,
-        //                                               object: nil)
-        //    }
-        //
-        //    deinit {
-        //        NotificationCenter.default.removeObserver(self)
-        //    }
-        //
-        //    @objc func keyboardNotification(notification: NSNotification) {
-        //        guard let userInfo = notification.userInfo else { return }
-        //
-        //        let endFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
-        //        let endFrameY = endFrame?.origin.y ?? 0
-        //        let duration:TimeInterval = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
-        //        let animationCurveRawNSN = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber
-        //        let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
-        //        let animationCurve:UIView.AnimationOptions = UIView.AnimationOptions(rawValue: animationCurveRaw)
-        //
-        //        if endFrameY >= UIScreen.main.bounds.size.height {
-        //            self.keyboardHeightLayoutConstraint?.constant = 0.0
-        //        } else {
-        //            self.keyboardHeightLayoutConstraint?.constant = endFrame?.size.height ?? 0.0
-        //        }
-        //
-        //        .animate(
-        //            withDuration: duration,
-        //            delay: TimeInterval(0),
-        //            options: animationCurve,
-        //            animations: { self.view.layoutIfNeeded() },
-        //            completion: nil)
-        //    }
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification: )), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+       
+    }
+                                               
+    @objc private func hideKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as?
+            NSValue {
+            
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            self.fieldsViewBottomConstraint.constant -= keyboardHeight
+            
+            // Animate Constraints
+            UIView.animate(withDuration: 0.5) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    @objc private func keyboardWillHide() {
+        self.fieldsViewBottomConstraint.constant = 0
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
