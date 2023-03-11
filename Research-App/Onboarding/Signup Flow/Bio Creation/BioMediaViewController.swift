a6b24de41f398b05a7a1c5a19779eb1833c9a6ce
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-03-11 09:31:25 -0500

    Interests Page Makeover

---
+++ Research-App/Onboarding/Signup Flow/Bio Creation/BioMediaViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BioMediaViewController.swift
+//  Research-App
+//
+//  Created by Levi Harris on 3/11/23.
+//
+
+import UIKit
+
+class BioMediaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
